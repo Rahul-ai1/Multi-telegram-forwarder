a85119170598f0d115c0e4584fd3b685a84a3ee2
Rahul <rc1248117@outlook.com>
2026-01-19 21:29:08 +0530

    Update telegram_forwarder.py changing port

--- telegram_forwarder.py
+++ telegram_forwarder.py
@@ -30,7 +30,8 @@ def home():
     return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0', port=8081)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     Thread(target=run).start()
